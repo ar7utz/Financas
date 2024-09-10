b847f51512c95c636c0a73918b8219bc73e29908
ar7utz <pereira.artur@hotmail.com>
2024-09-10 15:42:53 -0300

    adição das cores do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "!./node_modules",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tollens': '#005485',
+        'kansai':'#6f848c',
+        'resene':'#cbd4d4',
+        'ghostwhite':'#F8F8FF',
+      }
+    },
   },
   plugins: [],
 }
