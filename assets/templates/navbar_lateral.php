6babfdaf474afcb2ad8d5b9e435b63da5f803dc5
ar7utz <pereira.artur@hotmail.com>
2024-10-02 12:05:51 -0300

    comeco do planner e ajustes nos templates de nav

--- assets/templates/navbar_lateral.php
+++ assets/templates/navbar_lateral.php
@@ -1,20 +1,16 @@
 <link rel="stylesheet" href="../css/output.css">
 <div class="w-80 h-screen rounded-br-3xl bg-black">
     <div class="bg-red items-center justify-center text-center">
-        <div id="cardbtn1teste" class="w-48 h-14 text-center bg-slate-500 rounded-md">
-            <a href="">
-                <button class="justify-center">
-                    Dashboard
-                </button>
-            </a>
-        </div>
+        <a href="../../modulos/dashboard/dashboard.php">
+            <button id="cardbtn1teste" class="w-48 h-14 text-center mt-14 bg-slate-500 rounded-md">
+                Dashboard
+            </button>
+        </a>
 
-        <div id="cardbtn2teste" class="w-48 h-14 text-center mt-8 bg-slate-400 rounded-md">
-            <a href="">
-                <button class="">
-                    Planejador
-                </button>
-            </a>
-        </div>
+        <a href="../../modulos/planejador/planner.php">
+            <button id="cardbtn2teste" class="w-48 h-14 text-center mt-8 bg-slate-400 rounded-md">
+                Planejador
+            </button>
+        </a>
     </div>
 </div>
---
+++ modulos/planejador/planner.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once '../../assets/bd/conexao.php'
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/output.css">
+    <title>Planejador</title>
+</head>
+<body>
+    <?php include_once('../../assets/templates/navbar.php') ?>
+</body>
+</html>
