df89433caf92b9b14e5a31e88bb5207e38aa80d9
ar7utz <pereira.artur@hotmail.com>
2024-11-22 16:12:33 -0300

    :bug: concertando os caminhos das rotas

--- assets/templates/navbar.php
+++ assets/templates/navbar.php
@@ -2,20 +2,25 @@
 
 // Captura a URL da página anterior
 $url_anterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $url_base;
+$pagina_atual = basename($_SERVER['PHP_SELF']);
 
+$esconder_botao_voltar = ($pagina_atual == 'hp_login.php');
 ?>
 
 <header class="bg-tollens p-4 flex justify-between items-center">
-    <a href="<?php echo $url_anterior; ?>">
-        <button class="bg-gray-400 text-white py-2 px-4 rounded">Voltar</button>
-    </a>
+    <?php if (!$esconder_botao_voltar): ?>
+        <a href="<?php echo $url_anterior; ?>">
+            <button class="bg-gray-400 text-white py-2 px-4 rounded">Voltar</button>
+        </a>
+    <?php endif; ?>
+
     <div class="text-center justify-center items-center">
         <div class="text-white text-2xl text-center font-bold">
-            <img src="../logo/cube-logo.svg" alt="Logo" > 
+            <img src="<?php echo $url_base ?>/assets/logo/preview.svg" alt="Logo" width="60" height="70"> 
         </div>
     </div>
     <div class="space-x-2">
-        <a href="../usuario/perfil.php">
+        <a href="<?php $url_base ?>../usuario/perfil.php">
             <button class="bg-gray-400 text-white py-2 px-4 rounded">Meu Perfil</button>
         </a>
         <button id="abrirModal" class="bg-gray-400 text-white py-2 px-4 rounded">Sair</button>
@@ -25,7 +30,7 @@ $url_anterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $ur
         <div class="bg-white p-6 rounded-md text-center">
             <p class="mb-4">Tem certeza de que deseja sair?</p>
             <div class="flex justify-center space-x-4">
-                <a href="../login/logout.php">
+                <a href="<?php $url_base ?>../login/logout.php">
                     <button id="confirmarLogout" class="bg-green-600 text-white py-2 px-4 rounded hover:bg-green-500">Confirmar</button>
                 </a>
                 <button id="cancelarLogout" class="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500">Cancelar</button>
