e08e9f5f69c790a720af2aad34825348cf34daee
uflavio <flavio18hds@gmail.com>
2025-10-28 18:47:30 -0300

    Responsividade perfil

--- modulos/usuario/perfil.php
+++ modulos/usuario/perfil.php
@@ -55,38 +55,44 @@ if ($rowInvest = $resultInvest->fetch_assoc()) {
     <title>Finstash - Meu Perfil</title>
 </head>
 
-<body>
-    <?php include_once('../../assets/templates/navbar.php') ?>
+<body class="bg-gray-100">
+    <?php
+        include_once('../../assets/templates/navbar.php')
+    ?>
 
     <div class="w-4/5 mx-auto">
-        <div class="flex flex-row mt-8">
-            <!-- Div da foto - lado esquerdo -->
-            <div class="w-4/12 flex flex-col items-center">
-            <img src="../../assets/uploads/<?php echo $usuario['foto'] ? htmlspecialchars($usuario['foto']) : '../../assets/uploads/foto_default.png'; ?>" alt="Foto de perfil" class=" w-32 h-32 rounded-full object-cover">
-                <label class="bg-blue-500 text-white mt-4 px-4 py-2 rounded-md">Foto de perfil</label>
+        <div class="flex flex-col md:flex-row mt-8 bg-white p-6 rounded-lg shadow-md">
+            <div class="w-full md:w-1/3 flex flex-col items-center justify-center">
+                <!-- Div da foto - lado esquerdo -->
+                <div>
+                    <img src="../../assets/uploads/<?php echo $usuario['foto'] ? htmlspecialchars($usuario['foto']) : '../../assets/uploads/foto_default.png'; ?>"
+                     alt="Foto de perfil"
+                     class=" w-64 h-64 rounded-full object-cover">
+                    <!-- <label class="bg-blue-500 text-white mt-4 px-4 py-2 rounded-md">Foto de perfil</label> -->
+                </div>
             </div>
             <!-- Div das informações - lado direito -->
-            <div class="w-8/12 flex flex-col justify-center">
-                <div class="grid grid-cols-2 gap-4 mb-4">
+            <div class="w-full md:w-2/3 flex flex-col justify-center mt-5">
+                <div class="grid md:grid-cols-2 gap-4 mb-4">
                     <div class="flex flex-col">
                         <label for="nome">Nome:</label>
                         <input type="text" value="<?php echo htmlspecialchars($nome);?>" class="border p-2  rounded-md" disabled>
                     </div>
-                    <div class="">
+                    <div>
                         <label for="">Nome de usuário</label>
                         <input type="text" value="<?php echo htmlspecialchars($username);?>" class="border p-2 w-full rounded-md" disabled>
                     </div>
                 </div>
             
                 <div class="flex flex-col">
-                    <div class="">
+                    <div>
                         <label for="">Telefone</label>
                         <input type="text" value="<?php echo htmlspecialchars($telefone);?>" class="border p-2 w-full rounded-md" disabled>
                     </div>
                 </div>
 
                 <div class="flex flex-col">
-                    <div class="">
+                    <div>
                         <label for="">Email:</label>
                         <input type="text" value="<?php echo htmlspecialchars($email);?>" class="border p-2 w-full rounded-md" disabled>
                     </div>
@@ -99,14 +105,12 @@ if ($rowInvest = $resultInvest->fetch_assoc()) {
                     </div>
                 </div>
 
-                <div class="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-4 mt-2">
+                <div class="flex justify-center">
                     <a href="./editar_usuario.php"><button class="bg-blue-500 text-white mt-4 px-4 py-2 rounded-md">Editar Informações</button></a>
                 </div>
             </div>
         </div>
     </div>
-
-
 </body>
 
 </html>
